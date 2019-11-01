1e7b497d8bdb1f5f640fe90c47d6733a8139c8a3
Areg-Miqayelyan1 <mareg87@rambler.ru>
2019-11-01 15:36:09 +0400

    Updated 01.11.2019

---
+++ src/script.js
@@ -0,0 +1,58 @@
+const regUrl = `https://it-blog-posts.herokuapp.com/api/people`;
+const logUrl = `https://it-blog-posts.herokuapp.com/api/people/login`;
+
+const regSub = document.getElementById("regSub");
+const logSub = document.getElementById("logSub");
+
+function registration() {
+    const usernameInp = document.getElementById("usernameInp");
+    const lastnameInp = document.getElementById("lastnameInp");
+    const emailInp = document.getElementById("emailInp");
+    const passwordInp = document.getElementById("passwordInp");
+    let data = {
+        "lastname": `${usernameInp.value}`,
+        "username": `${lastnameInp.value}`,
+        "password": `${emailInp.value}`,
+        "email": `${passwordInp.value}`
+    }
+    fetch(regUrl, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(() => window.open("login.html", "_self"))
+    .catch(error => console.log(error))
+}
+
+function login() {
+    const logEmail = document.getElementById("logEmail");
+    const logPassword = document.getElementById("logPassword");
+    let data = {
+        "password": `${logPassword.value}`,
+        "email": `${logEmail.value}`
+    };
+    fetch(logUrl, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    // .then(data => {
+    //     fetch(`https://it-blog-posts.herokuapp.com/api/meetups?access_token=${data.id}`)
+    // })
+    .then(() => window.open("workspace.html", "_self"))
+    .catch(error => alert(error))
+}
+
+if(regSub) {
+    regSub.addEventListener("click", registration)
+}
+
+if(logSub) {
+    logSub.addEventListener("click", login)
+}
